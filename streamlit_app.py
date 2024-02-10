96e55923b4b5135fcb366dd2400959e44d11c0ee
frla18cz <70630255+frla18cz@users.noreply.github.com>
2024-02-10 17:37:03 +0100

    update test 1.01

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,24 +16,10 @@ from modules.login import login
 
 
 
-local_debug_mode = True
+local_debug_mode = False
+st.echo(f"local_debug_mode: {local_debug_mode}")
 assistant_id = login(local_debug_mode)
 client = openai
-
-
-# if local_debug_mode == True:
-#     config = toml.load("secrets.toml")
-#     openai.api_key = config['api']['API_KEY']
-#     assistant_id = config['api']['ASSISTANT_ID']
-#     print(f"Připojení k OpenAI API lokálním klíčem")
-# else:
-#     #Inicializace api key a ID. Uloženo na cloudu streamlit v secret
-#     openai.api_key = st.secrets["API_KEY"]
-#     assistant_id = st.secrets["ASSISTANT_ID"]
-#     # assistant_id = "asst_atZWsxED84ngEs7lXxCAKR9Q" #Pro testovací účely, light prompt
-#     print("Připojení k OpenAI API secret klíčem na cloudu streamlit")
-#
-# client = openai
 
 
 
